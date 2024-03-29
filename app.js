398333845e28d00df78f066ad472b3b9f354b3db
alken1t15 <alken1t15@gmail.com>
2024-03-29 23:42:53 +0500

    practice 2

--- app.js
+++ app.js
@@ -1,22 +1,42 @@
 const App = {
-    data(){
-        return{
-            placeholderString:'Введите название заметок',
-            title:'Список заметок',
-            inputValue:'',
-            notes: ['Заметка 1','Заметка 2']
+    data() {
+        return {
+            placeholderString: 'Введите название заметок',
+            title: 'Список заметок',
+            inputValue: '',
+            notes: ['Заметка 1', 'Заметка 2']
         }
     },
-    methods:{
-        inputChangeHandler(event){
-            this.inputValue=event.target.value
+    methods: {
+        // inputChangeHandler(event) {
+        //     this.inputValue = event.target.value
+        // },
+        addNewNote() {
+            if (this.inputValue!=='') {
+                this.notes.push(this.inputValue)
+                this.inputValue = ''
+            }
         },
-        addNewNote(){
-            this.notes.push(this.inputValue)
-            this.inputValue=''
+        toUpperCase(item){
+          return item.toUpperCase();
         },
-        removeNote(idx){
-            this.notes.splice(idx,1)
+        removeNote(idx) {
+            this.notes.splice(idx, 1)
+        }
+    }
+    ,
+    computed:{
+        doubleCountComputed(){
+            return this.notes.length * 2;
+        },
+    },
+    watch:{
+        inputValue(value){
+            if (value.length > 10){
+                this.inputValue = ''
+            }
+            console.log(value)
+
         }
     }
 }
