9284b01dcf1ac1a3f58114c504189e8e07688b7b
alken1t15 <alken1t15@gmail.com>
2024-04-28 23:51:34 +0500

    practice 3

--- app.js
+++ app.js
@@ -19,6 +19,16 @@ Vue.createApp({
     methods: {
         stopPropagation(event) {
             event.stopPropagation()
+        },
+        addItem(){
+            this.items.unshift(this.$refs.myInput.value)
+            this.$refs.myInput.value = ''
+        },
+        remove(i){
+            this.items.splice(i,1)
+        },
+        log(item){
+            console.log('Log item: ', item)
         }
     }
 }).mount('#app')
